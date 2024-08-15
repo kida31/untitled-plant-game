f914813e6510947c762dfafd30c15eac8db0d675
lavenderaddict <leasing-07-adagio@icloud.com>
2024-08-15 17:19:23 +0200

    Add player state machine (#6)
    Co-authored-by: Zhang Elisa <78491387+Sui-nyan@users.noreply.github.com>
    resolves #5

---
+++ Player/Player.cs
@@ -0,0 +1,76 @@
+using System;
+using Godot;
+
+namespace untitledplantgame.Player;
+
+public partial class Player : CharacterBody2D
+{
+    private Vector2 _cardinalDirection = Vector2.Down;
+    public Vector2 Direction = Vector2.Zero;
+    
+    private AnimatedSprite2D _animatedSprite2D;
+    private PlayerStateMachine _stateMachine;
+    
+    public override void _Ready()
+    {
+        _stateMachine = GetNode<PlayerStateMachine>("StateMachine");
+        _animatedSprite2D = GetNode<AnimatedSprite2D>("AnimatedSprite2D");
+        _stateMachine.Initialize(this);
+    }
+
+    public override void _Process(double delta)
+    {
+        Direction.X = Input.GetActionStrength("right") - Input.GetActionStrength("left");
+        Direction.Y = Input.GetActionStrength("down") - Input.GetActionStrength("up");
+
+        //Velocity = direction * MoveSpeed;
+    }
+
+    public override void _PhysicsProcess(double delta)
+    {
+        MoveAndSlide();
+    }
+
+    public bool SetDirection()
+    {
+        Vector2 newDirection = _cardinalDirection;
+
+        if (Direction == Vector2.Zero) return false;
+
+        if (Direction.Y == 0)
+        {
+            if (Direction.X < 0) newDirection = Vector2.Left;
+            else newDirection = Vector2.Right;
+        }
+
+        if (Direction.X == 0)
+        {
+            if (Direction.Y > 0) newDirection = Vector2.Down;
+            else newDirection = Vector2.Up;
+        }
+
+        _cardinalDirection = newDirection;
+        return true;
+    }
+
+    public void UpdateAnimation(string state)
+    {
+        var animationState = state + "_" + AnimationDirection();
+        _animatedSprite2D.Play(animationState);
+    }
+
+    string AnimationDirection()
+    {
+        if (_cardinalDirection == Vector2.Down)
+            return "down";
+        if (_cardinalDirection == Vector2.Up)
+            return "up";
+        if (_cardinalDirection == Vector2.Left)
+            return "left";
+        if (_cardinalDirection == Vector2.Right)
+            return "right";
+
+        throw new InvalidOperationException("Invalid direction; Unreachable Code");
+    }
+    
+}
---
+++ Player/PlayerStateMachine.cs
@@ -0,0 +1,65 @@
+using Godot;
+using System.Collections.Generic;
+
+namespace untitledplantgame.Player; 
+
+public partial class PlayerStateMachine : Node
+{
+    private List<State> _states;
+    private State _prevState;
+    private State _currState;
+
+    public override void _Ready()
+    {
+        ProcessMode = ProcessModeEnum.Disabled;
+    }
+
+    public override void _Process(double delta)
+    {
+        ChangeState(_currState.Process(delta));
+    }
+
+    public override void _PhysicsProcess(double delta)
+    {
+        ChangeState(_currState.Physics(delta));
+    }
+
+    public override void _UnhandledInput(InputEvent @event)
+    {
+        ChangeState(_currState.HandleInput(@event));
+    }
+
+    public void Initialize(Player player)
+    {
+        _states = new List<State>();
+
+        foreach (var child in GetChildren())
+        {
+            if (child.GetType().IsSubclassOf(typeof(State)))
+            {
+                var state = (State)child;
+                state.Player = player;
+                _states.Add((State)child);
+            }
+        }
+
+        if (_states.Count > 0)
+        {
+            ChangeState(_states[0]);
+            ProcessMode = ProcessModeEnum.Inherit;
+        }
+        
+    }
+
+    void ChangeState(State newState)
+    {
+        if (newState == null || newState == _currState) return;
+
+        if (_currState != null)
+            _currState.Exit();
+
+        _prevState = _currState;
+        _currState = newState;
+        _currState.Enter();
+    }
+}
