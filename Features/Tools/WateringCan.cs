ff344a9b37257024d3733ad7fcaea23617f2bbd8
kida31 <83455653+kida31@users.noreply.github.com>
2024-11-29 16:53:20 +0100

    Apply watering to closest soil only

--- Features/Tools/WateringCan.cs
+++ Features/Tools/WateringCan.cs
@@ -1,50 +1,68 @@
+using System;
 using System.Linq;
 using Godot;
 using untitledplantgame.Common;
 using untitledplantgame.Plants.Soil;
-using untitledplantgame.Player;
-using untitledplantgame.Tools;
+
+namespace untitledplantgame.Tools;
 
 public class WateringCan : Tool
 {
-    // Placeholders
-    private const int PlaceholderWateringAmount = 20;
-    private const float WateringRadius = 16;
-    private const float WateringRange = 16; // TODO: Fix; Direction of player is only active while moving instead of most recent facing
-    // EndPlaceholders
+	// Placeholders
+	private const int PlaceholderWateringAmount = 20;
+	private const float WateringRadius = 16;
 
-    private readonly float _wateringAmount;
-    private readonly Logger _logger;
-    public WateringCan() : this(WateringRadius, WateringRange)
-    {
-	    _wateringAmount = PlaceholderWateringAmount;
-    }
+	private const float WateringRange = 16; // TODO: Fix; Direction of player is only active while moving instead of most recent facing
+	// EndPlaceholders
 
-    public WateringCan(float radius, float range) : base(radius, range)
-    {
-        _logger = new Logger("WateringCan");
-    }
+	private readonly float _wateringAmount;
+	private readonly float _waterCapacity;
+	private readonly bool _isBottomless;
 
-    protected override bool OnHit(Player user, Node2D[] hits)
-    {
-        var soil = hits.OfType<IWaterable>().FirstOrDefault();
-        if (soil == null)
-        {
-            return false;
-        }
+	private float _currentWaterLevel;
+	private readonly Logger _logger;
 
-        _logger.Debug("Watering soil");
-        soil.AddWater(_wateringAmount);
-        return true;
-    }
+	public WateringCan() : this(WateringRadius, WateringRange)
+	{
+		_wateringAmount = PlaceholderWateringAmount;
+	}
 
-    protected override void OnMiss(Player user)
-    {
-        _logger.Debug("Did not water anything. Waste of water");
-    }
+	public WateringCan(float radius, float range) : base(radius, range)
+	{
+		_logger = new Logger("WateringCan");
+	}
 
-    protected override void OnUse(Player user)
-    {
-        // Something. Example reduce water in tool
-    }
+	protected override bool OnHit(Player.Player user, Node2D[] hits)
+	{
+		var closestSoil = hits
+			.OfType<IWaterable>()
+			.MinBy(o => (o as Node2D)!.GlobalPosition.DistanceSquaredTo(user.GlobalPosition));
+
+		if (closestSoil == null)
+		{
+			return false;
+		}
+
+		_logger.Debug("Watering soil");
+		var deltaWater = Math.Min(_wateringAmount, _currentWaterLevel);
+		closestSoil.AddWater(deltaWater);
+		
+		// Consider moving this to OnUse instead
+		if (!_isBottomless)
+		{
+			_currentWaterLevel -= deltaWater;
+		}
+
+		return true;
+	}
+
+	protected override void OnMiss(Player.Player user)
+	{
+		_logger.Debug("Did not water anything. Waste of water");
+	}
+
+	protected override void OnUse(Player.Player user)
+	{
+		// Something. Example reduce water in tool
+	}
 }
