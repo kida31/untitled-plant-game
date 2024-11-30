2c8974dbe53ae961d0172f56a1de6d4ce104390b
kida31 <83455653+kida31@users.noreply.github.com>
2024-11-30 21:54:58 +0100

    Add more tool assets. Connect toolbelt view to game
    Co-Authored-By: Elisa <78491387+Sui-nyan@users.noreply.github.com>

--- Features/GUI/HUDs/Toolbelt/ToolBlobView.cs
+++ Features/GUI/HUDs/Toolbelt/ToolBlobView.cs
@@ -1,5 +1,6 @@
 using Godot;
 using System;
+using untitledplantgame.Tools;
 
 public partial class ToolBlobView : MarginContainer
 {
@@ -11,8 +12,6 @@ public partial class ToolBlobView : MarginContainer
 		Secondary
 	}
 
-	[Export] private AnimationPlayer _animationPlayer;
-
 	[Export]
 	public Style BlobStyle
 	{
@@ -20,16 +19,39 @@ public partial class ToolBlobView : MarginContainer
 		set => SetBlobStyle(value);
 	}
 
-	[Export]
-	public Style InstantBlobStyle
-	{
-		get => _style;
-		set => SetBlobStyle(value, customSpeed: Single.MaxValue);
+	[Export] private AnimationPlayer _animationPlayer;
+	[Export] private TextureRect _toolIcon;
+	
+	[ExportGroup("Placeholder hardcoded icons")]
+	[Export] private Texture2D _wateringCanIcon;
+	[Export] private Texture2D _shovelIcon;
+	[Export] private Texture2D _shearsIcon;
+
+	public Tool Tool {
+		get => _tool;
+		set
+		{
+			_tool = value;
+			_toolIcon.Texture = _tool switch
+			{
+				// Placeholder
+				WateringCan => _wateringCanIcon,
+				Shears => _shearsIcon,
+				null => null,
+				_ => _shovelIcon
+			};
+		}
 	}
 
+	private Tool _tool;
 	private Style _style = Style.Primary;
 
-	private void SetBlobStyle(Style value, float customSpeed = 1.0f)
+	public override void _Ready()
+	{
+		UpdateVisuals();
+	}
+
+	private void SetBlobStyle(Style value)
 	{
 		if (value == _style)
 		{
@@ -37,14 +59,18 @@ public partial class ToolBlobView : MarginContainer
 		}
 
 		_style = value;
+		UpdateVisuals(1.0f);
+	}
 
+	private void UpdateVisuals(float customSpeed = Single.MaxValue)
+	{
 		if (_style == Style.Primary)
 		{
-			_animationPlayer.Play(ToPrimaryAnimationName, customSpeed: customSpeed);
+			_animationPlayer?.Play(ToPrimaryAnimationName, customSpeed: customSpeed);
 		}
 		else
 		{
-			_animationPlayer.Play(ToPrimaryAnimationName, customSpeed: -customSpeed, fromEnd: true);
+			_animationPlayer?.Play(ToPrimaryAnimationName, customSpeed: -customSpeed, fromEnd: true);
 		}
 	}
 }
