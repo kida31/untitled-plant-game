a8f57a62df11fe1fba2d69f37b2653928fee83eb
kida31 <83455653+kida31@users.noreply.github.com>
2024-12-05 15:37:55 +0100

    Add tabbing per buttons. Rename/reorder stuff

--- Features/GUI/Book/Inventories/InventoryTabsView.cs
+++ Features/GUI/Book/Inventories/InventoryTabsView.cs
@@ -3,6 +3,7 @@ using Godot;
 using System.Collections.Generic;
 using System.Linq;
 using untitledplantgame.Common;
+using untitledplantgame.Common.Inputs.GameActions;
 using untitledplantgame.Inventory.PlayerInventory.UI_InventoryItem;
 using untitledplantgame.Inventory.PlayerInventory.Views;
 
@@ -11,8 +12,11 @@ namespace untitledplantgame.Inventory.PlayerInventory.UI_Tabs;
 public partial class InventoryTabsView : Control
 {
 	[Export] private PackedScene _inventoryViewPrefab;
-	private InventoryItemView _potentialItemSlot;
+	[Export] private TabContainer _tabContainer;
 
+	// public TabContainer TabContainer => GetNode<TabContainer>("."); // Either this or make this class extend TabContainer
+
+	private InventoryItemView _potentialItemSlot;
 	private List<InventoryView> _tabs;
 
 
@@ -26,7 +30,53 @@ public partial class InventoryTabsView : Control
 
 		_tabs ??= GetChildren().OfType<InventoryView>().ToList();
 	}
+	
+	public override void _UnhandledInput(InputEvent @event)
+	{
+		if (!Visible)
+		{
+			return;
+		}
+		
+		if (@event.IsActionPressed(Book.BumperLeft))
+		{
+			PreviousTab(false);
+		}
+		else if (@event.IsActionPressed(Book.BumperRight))
+		{
+			NextTab(false);
+		}
+	}
 
+	private void PreviousTab(bool wrapAround = true)
+	{
+		var tabContainer = _tabContainer;
+		if (wrapAround)
+		{
+			var nextOrFirstTabIndex = (tabContainer.CurrentTab + 1) % tabContainer.GetChildCount();
+			tabContainer.CurrentTab = nextOrFirstTabIndex;
+		}
+		else
+		{
+			tabContainer.CurrentTab = Math.Max(0, tabContainer.CurrentTab - 1);
+		}
+
+	}
+	
+	private void NextTab(bool wrapAround = true)
+	{
+		var tabContainer = _tabContainer;
+		var count = tabContainer.GetChildCount();
+		if (wrapAround)
+		{
+			tabContainer.CurrentTab = (tabContainer.CurrentTab + count - 1) % count;
+		}
+		else
+		{
+			tabContainer.CurrentTab = Mathf.Min(tabContainer.CurrentTab + 1, count - 1);
+		}
+	}
+	
 	public void UpdateInventories(List<IInventory> inventories)
 	{
 		// Remove excess tabs
@@ -45,7 +95,7 @@ public partial class InventoryTabsView : Control
 			AddChild(newTab);
 			_tabs.Add(newTab);
 		}
-		
+
 		Assert.AssertEquals(inventories.Count, _tabs.Count, "Inventories and tabs count mismatch");
 		for (int i = 0; i < inventories.Count; i++)
 		{
--- Features/GUI/Book/Pages/PlayerInventoryPage.cs
+++ Features/GUI/Book/Pages/PlayerInventoryPage.cs
@@ -1,5 +1,7 @@
+using System;
 using System.Collections.Generic;
 using Godot;
+using untitledplantgame.Common.Inputs.GameActions;
 using untitledplantgame.Inventory.PlayerInventory.UI_Tabs;
 
 namespace untitledplantgame.Inventory.PlayerInventory;
