8cd40cdead5229cd42b818a81bb265461e93548b
kida31 <83455653+kida31@users.noreply.github.com>
2024-12-05 16:53:28 +0100

    Add tab buttons

--- Features/GUI/Book/Inventories/InventoryTabsView.cs
+++ Features/GUI/Book/Inventories/InventoryTabsView.cs
@@ -4,6 +4,7 @@ using System.Collections.Generic;
 using System.Linq;
 using untitledplantgame.Common;
 using untitledplantgame.Common.Inputs.GameActions;
+using untitledplantgame.Inventory.GUI;
 using untitledplantgame.Inventory.PlayerInventory.UI_InventoryItem;
 using untitledplantgame.Inventory.PlayerInventory.Views;
 
@@ -12,13 +13,18 @@ namespace untitledplantgame.Inventory.PlayerInventory.UI_Tabs;
 public partial class InventoryTabsView : Control
 {
 	[Export] private PackedScene _inventoryViewPrefab;
+	[Export] private PackedScene _inventoryCategoryTabPrefab;
+
 	[Export] private TabContainer _tabContainer;
+	[Export] private Container _tabButtonContainer;
 
 	// public TabContainer TabContainer => GetNode<TabContainer>("."); // Either this or make this class extend TabContainer
 
 	private InventoryItemView _potentialItemSlot;
+	private List<InventoryCategoryTab> _tabButtons;
 	private List<InventoryView> _tabs;
 
+	private Dictionary<InventoryCategoryTab, Action> _buttonActions = new();
 
 	public override void _Ready()
 	{
@@ -28,16 +34,37 @@ public partial class InventoryTabsView : Control
 		EventBus.Instance.OnSetItemSlot += SetPotentialItemSlot;
 		EventBus.Instance.OnGetItemSlot += GetPotentialItemSlot;
 
-		_tabs ??= GetChildren().OfType<InventoryView>().ToList();
+		_tabs = _tabContainer.GetChildren().OfType<InventoryView>().ToList();
+		_tabButtons = _tabButtonContainer.GetChildren().OfType<InventoryCategoryTab>().ToList();
+
+		_tabContainer.TabChanged += OnTabChanged;
 	}
-	
+
+	private void OnTabChanged(long tab)
+	{
+		int index = (int) tab;
+		if (index < 0 || index >= _tabs.Count)
+		{
+			// log error
+			return;
+		}
+
+		for (var i = 0; i < _tabButtons.Count; i++)
+		{
+			var s = i == index ? "active" : "inactive";
+			GD.Print($"{i} as {s}");
+			var button = _tabButtons[index];
+			button.SetIsActive(i == index);
+		}
+	}
+
 	public override void _UnhandledInput(InputEvent @event)
 	{
 		if (!Visible)
 		{
 			return;
 		}
-		
+
 		if (@event.IsActionPressed(Book.BumperLeft))
 		{
 			PreviousTab(false);
@@ -60,9 +87,8 @@ public partial class InventoryTabsView : Control
 		{
 			tabContainer.CurrentTab = Math.Max(0, tabContainer.CurrentTab - 1);
 		}
-
 	}
-	
+
 	private void NextTab(bool wrapAround = true)
 	{
 		var tabContainer = _tabContainer;
@@ -76,33 +102,75 @@ public partial class InventoryTabsView : Control
 			tabContainer.CurrentTab = Mathf.Min(tabContainer.CurrentTab + 1, count - 1);
 		}
 	}
-	
+
 	public void UpdateInventories(List<IInventory> inventories)
 	{
-		// Remove excess tabs
+		// Remove excess tabs and buttons
 		while (_tabs.Count > inventories.Count)
 		{
 			var tabToRemove = _tabs.Last();
 			_tabs.Remove(tabToRemove);
-			RemoveChild(tabToRemove);
+			_tabContainer.RemoveChild(tabToRemove);
 			tabToRemove.QueueFree();
+		}
+
+		while (_tabButtons.Count > inventories.Count)
+		{
+			var buttonToRemove = _tabButtons.Last();
+			_tabButtons.Remove(buttonToRemove);
+			_tabButtonContainer.RemoveChild(buttonToRemove);
+			buttonToRemove.QueueFree();
 		}
 
 		// Add tabs based on the inventories list
 		while (_tabs.Count < inventories.Count)
 		{
 			var newTab = _inventoryViewPrefab.Instantiate<InventoryView>();
-			AddChild(newTab);
+			_tabContainer.AddChild(newTab);
 			_tabs.Add(newTab);
+		}
+
+		while (_tabButtons.Count < inventories.Count)
+		{
+			var newButton = _inventoryCategoryTabPrefab.Instantiate<InventoryCategoryTab>();
+			_tabButtonContainer.AddChild(newButton);
+			_tabButtons.Add(newButton);
 		}
 
 		Assert.AssertEquals(inventories.Count, _tabs.Count, "Inventories and tabs count mismatch");
 		for (int i = 0; i < inventories.Count; i++)
 		{
+			// Tab
 			var inventory = inventories[i];
 			var tab = _tabs[i];
 			tab.UpdateInventory(inventory);
 			tab.Name = inventory.Name;
+
+			//Button
+			var button = _tabButtons[i];
+			button.Name = inventory.Name;
+			button.Text = inventory.Name;
+			if (_buttonActions.ContainsKey(button))
+			{
+				button.Pressed -= _buttonActions[button];
+			}
+
+			void ButtonAction() => OnTabButtonPressed(button);
+			_buttonActions[button] = ButtonAction;
+			button.Pressed += ButtonAction;
+		}
+	}
+
+	private void OnTabButtonPressed(InventoryCategoryTab button)
+	{
+		var index = _tabButtons.IndexOf(button);
+		if (index < 0)
+		{
+			// Log error
+		}
+		else
+		{
+			_tabContainer.CurrentTab = index;
 		}
 	}
 
---
+++ Features/GUI/Book/InventoryCategoryTab.cs
@@ -0,0 +1,67 @@
+using Godot;
+using System;
+using untitledplantgame.Inventory.GUI;
+
+[Tool]
+public partial class InventoryCategoryTab : Control, INavigatable
+{
+	private const string ActivateAnimation = "Activate";
+	public event Action Pressed;
+	
+	[Export]
+	public string Text
+	{
+		get => _textLabel.Text ?? "";
+		set
+		{
+			if (_textLabel != null)
+			{
+				_textLabel.Text = value;	
+			}
+		}
+	}
+
+	[Export]
+	public bool IsActive
+	{
+		get => _isActive;
+		private set
+		{
+			// if is editor, skip animation
+			if (Engine.IsEditorHint())
+			{
+				SetIsActive(value, true);
+			}
+			else
+			{
+				SetIsActive(value);	
+			}
+		}
+	}
+	
+	[ExportGroup("Hidden")]
+	[Export] private TextureRect _activeIcon;
+	[Export] private TextureRect _inactiveIcon;
+	[Export] private Label _textLabel;
+	[Export] private AnimationPlayer _animationPlayer;
+
+	private bool _isActive;
+
+	public void SetIsActive(bool value, bool instantAnimation = false)
+	{
+		_isActive = value;
+		
+		if (_isActive)
+		{
+			_animationPlayer.Play(ActivateAnimation);
+			GD.Print(Name + _isActive);
+			// Play animation
+		}
+		else
+		{
+			_animationPlayer.PlayBackwards(ActivateAnimation);
+			// Play animation
+			GD.Print(Name + _isActive);
+		}
+	}
+}
