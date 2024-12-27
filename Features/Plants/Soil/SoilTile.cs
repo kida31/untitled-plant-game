b3f060dd335203e9c7470e17e7ae65db4660eb1c
Elisa Z. <ez018@hdm-stuttgart.de>
2024-12-27 16:03:01 +0100

    Soil reacts to weather

--- Features/Plants/Soil/SoilTile.cs
+++ Features/Plants/Soil/SoilTile.cs
@@ -1,14 +1,51 @@
 ﻿using System;
 using Godot;
+using untitledplantgame.Common;
+using untitledplantgame.Cycle.Weather;
 
 namespace untitledplantgame.Plants.Soil;
 
 public partial class SoilTile : Area2D, IWaterable
 {
-	[Export]
-	public float Hydration { get; private set; }
-	private float _maxHydration = 200;
+	[Export] public float Hydration { get; private set; }
+	private float _maxHydration = 250;
 	private float Fertilization { get; set; }
+	private Logger _logger;
+
+	public override void _Ready()
+	{
+		_logger = new Logger(this);
+		WeatherCycle.Instance.WeatherChanged += OnWeatherChanged;
+		TimeController.Instance.DayChanged += OnDayChanged;
+	}
+
+	private void OnDayChanged(int day)
+	{
+		var weather = WeatherCycle.Instance.CurrentWeather;
+		OnWeatherChanged(weather);
+	}
+
+	private void OnWeatherChanged(Weather obj)
+	{
+		switch (obj)
+		{
+			case Weather.Sunny:
+				EvaporateWater(30);
+				break;
+			case Weather.Rainy:
+				AddWater(100);
+				break;
+			case Weather.Snowy:
+				AddWater(50);
+				break;
+			case Weather.Cloudy:
+				EvaporateWater(20);
+				break;
+			default:
+				_logger.Warn("Soil is confused about the weather.");
+				return;
+		}
+	}
 
 	public float WithdrawHydration(float reductionValue)
 	{
@@ -24,8 +61,8 @@ public partial class SoilTile : Area2D, IWaterable
 	}
 
 	//Do we want this?
-	public void EvaporateWater()
+	public void EvaporateWater(int amount)
 	{
-		Hydration--;
+		Hydration = Math.Max(Hydration - amount, 0);
 	}
 }
