09bef335f849b71f306fe85be819a343e3ca12d7
Elisa Z. <ez018@hdm-stuttgart.de>
2024-12-28 18:48:20 +0100

    logging overlay for testing

---
+++ InGameLogOverlay.cs
@@ -0,0 +1,29 @@
+using Godot;
+using System.Collections.Generic;
+using untitledplantgame.Common;
+
+public partial class InGameLogOverlay : RichTextLabel
+{
+	private Queue<string> _logQueue = new ();
+	
+	public override void _Ready()
+	{
+		// Subscribe to the logger's events
+		Logger.MessageLogged += OnLogged;
+	}
+
+	private void OnLogged(string obj)
+	{
+		_logQueue.Enqueue(obj);
+		if (_logQueue.Count > 5)
+		{
+			_logQueue.Dequeue();
+		}
+	}
+
+	// Called every frame. 'delta' is the elapsed time since the previous frame.
+	public override void _Process(double delta)
+	{
+		Text = string.Join("\n", _logQueue);
+	}
+}
