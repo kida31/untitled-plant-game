2438dadfbf395c83d5586a6b43127ffbe802203e
kida31 <83455653+kida31@users.noreply.github.com>
2024-12-28 23:11:10 +0100

    Fix book visibility. Make wiki article null safe and add related items

--- Features/GUI/Book/Wiki/WikiArticleView.cs
+++ Features/GUI/Book/Wiki/WikiArticleView.cs
@@ -1,17 +1,41 @@
+using System;
+using System.Linq;
 using Godot;
+using untitledplantgame.Database;
 
 namespace untitledplantgame.Inventory.PlayerInventory.UI_Wiki;
 
 public partial class WikiArticleView : Node
 {
+	public event Action<IItemStack> RelatedItemClicked;
+
 	[Export] private TextureRect _iconTextureRect;
 	[Export] private Label _itemNameAndCategory;
 	[Export] private Label _itemDescription;
 	[Export] private Label _itemStats;
+	/// <summary>
+	/// These control objects need to have obj.Texture
+	/// </summary>
+	[Export] private Clickable[] _relatedItemClickables = new Clickable[0];
+
 	// TODO show related items
 	
 	private IItemStack _itemStack;
-	
+
+	public override void _Ready()
+	{
+		for(var i = 0; i < _relatedItemClickables.Length; i++){
+			var clickable = _relatedItemClickables[i];
+			var capturedIndex = i;
+			void OnPressHandler() {
+				var itemId = (_itemStack as ItemStack)?.RelatedItemIds[capturedIndex];
+				RelatedItemClicked?.Invoke(itemId != null ? ItemDatabase.Instance.CreateItemStack(itemId) : null);
+			}
+
+			clickable.Pressed += OnPressHandler;
+		}
+	}
+
 	public void UpdateItemStack(IItemStack itemStack)
 	{
 		_itemStack = itemStack;
@@ -21,9 +45,22 @@ public partial class WikiArticleView : Node
 
 	private void UpdateView()
 	{
-		_iconTextureRect.Texture = _itemStack.Icon;
-		_itemNameAndCategory.Text = $"{_itemStack.Name} - {_itemStack.Category.Name}";
-		_itemDescription.Text = _itemStack.Description;
+		_iconTextureRect.Texture = _itemStack?.Icon ?? null;
+		_itemNameAndCategory.Text = _itemStack != null ? $"{_itemStack.Name} - {_itemStack.Category.Name}": "";
+		_itemDescription.Text = _itemStack?.Description ?? "";
 		_itemStats.Text = "Stats!... \nStats!... \nStats!...";
+
+		var relatedItems = (_itemStack as ItemStack).RelatedItemIds
+			.Select(id => ItemDatabase.Instance.CreateItemStack(id)).ToList();
+
+		for (int i = 0; i < _relatedItemClickables.Length; i++)
+		{
+			// TODO: Type-safe this
+			var textRect = _relatedItemClickables[i].GetChild<TextureRect>(0);
+			
+			var item = i < relatedItems.Count ? relatedItems[i] : null;
+			textRect.Texture = item?.Icon ?? null;
+		}
 	}
+
 }
