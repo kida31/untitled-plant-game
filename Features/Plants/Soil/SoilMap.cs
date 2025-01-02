f184d513a1c21924ea799e3ed83db680d3ac3c36
lavenderaddict <leasing-07-adagio@icloud.com>
2025-01-02 23:07:25 +0100

    add 50 shades of soil
    actually, 32, but really just 24
    #150

--- Features/Plants/Soil/SoilMap.cs
+++ Features/Plants/Soil/SoilMap.cs
@@ -1,3 +1,4 @@
+using System;
 using Godot;
 using Godot.Collections;
 using untitledplantgame.Common;
@@ -6,7 +7,7 @@ namespace untitledplantgame.Plants;
 
 public partial class SoilMap : Node2D
 {
-	private const int TileSetSourceId = 2;
+	private const int TileSetSourceId = 0;
 
 	[Export] private Node2D _tester;
 	private TileMapLayer _tileLayer;
@@ -20,12 +21,12 @@ public partial class SoilMap : Node2D
 		Flooded
 	}
 	
-	private Dictionary<SoilHydration, Vector2I> _hydrationTileMap = new()
+	private Dictionary<SoilHydration, Array<Vector2I>> _hydrationTileMap = new()
 	{
-		{SoilHydration.Dry, new Vector2I(1, 0)},
-		{SoilHydration.Moist, new Vector2I(2, 0)},
-		{SoilHydration.Wet, new Vector2I(3, 0)},
-		{SoilHydration.Flooded, new Vector2I(4, 0)}
+		{SoilHydration.Dry, new Array<Vector2I>{new Vector2I(0, 0)}},
+		{SoilHydration.Moist, new Array<Vector2I>{new Vector2I(1, 0), new Vector2I(1, 1), new Vector2I(1, 2), new Vector2I(1, 3), new Vector2I(1, 4), new Vector2I(1, 5), new Vector2I(1, 6), new Vector2I(1, 7)}},
+		{SoilHydration.Wet, new Array<Vector2I>{new Vector2I(2, 0), new Vector2I(2, 1), new Vector2I(2, 2), new Vector2I(2, 3), new Vector2I(2, 4), new Vector2I(2, 5), new Vector2I(2, 6), new Vector2I(2, 7)}},
+		{SoilHydration.Flooded, new Array<Vector2I>{new Vector2I(3, 0), new Vector2I(3, 1), new Vector2I(3, 2), new Vector2I(3, 3), new Vector2I(3, 4), new Vector2I(3, 5), new Vector2I(3, 6), new Vector2I(3 , 7)}}
 	};
 	
 	public override void _Ready()
@@ -54,7 +55,7 @@ public partial class SoilMap : Node2D
 		_logger.Debug($"Tester position: {pos.X:F1}, {pos.Y:F1}");
 		var tilePos = _tileLayer.LocalToMap(pos);
 		_logger.Debug($"Test map position: {tilePos}");
-		_tileLayer.SetCell(tilePos, TileSetSourceId, _hydrationTileMap[SoilHydration.Dry]);
+		_tileLayer.SetCell(tilePos, TileSetSourceId, _hydrationTileMap[SoilHydration.Dry][0]);
 		//end testing
 		
 		
@@ -62,22 +63,24 @@ public partial class SoilMap : Node2D
 		_logger.Debug($"Soil Tile position: {tilePosition.X:F1}, {tilePosition.Y:F1}");
 		var tileMapPosition = _tileLayer.LocalToMap(tilePosition);
 		_logger.Debug($"Tile map position: {tileMapPosition}");
+		var randomSoilVariation = new Random();
+		
 		switch (hydration)
 		{
 			case < (int) SoilHydration.Dry:
-				_tileLayer.SetCell(tileMapPosition, TileSetSourceId, _hydrationTileMap[SoilHydration.Dry]);
+				_tileLayer.SetCell(tileMapPosition, TileSetSourceId, _hydrationTileMap[SoilHydration.Dry][0]);
 				_logger.Debug($"Tile with hydration {hydration} is dry");
 				break;
 			case < (int) SoilHydration.Moist:
-				_tileLayer.SetCell(tileMapPosition, TileSetSourceId, _hydrationTileMap[SoilHydration.Moist]);
+				_tileLayer.SetCell(tileMapPosition, TileSetSourceId, _hydrationTileMap[SoilHydration.Moist][randomSoilVariation.Next(8)]);
 				_logger.Debug($"Tile with hydration {hydration} is moist");
 				break;
 			case < (int) SoilHydration.Wet:
-				_tileLayer.SetCell(tileMapPosition, TileSetSourceId, _hydrationTileMap[SoilHydration.Wet]);
+				_tileLayer.SetCell(tileMapPosition, TileSetSourceId, _hydrationTileMap[SoilHydration.Wet][randomSoilVariation.Next(8)]);
 				_logger.Debug($"Tile with hydration {hydration} is wet");
 				break;
 			default:
-				_tileLayer.SetCell(tileMapPosition, TileSetSourceId, _hydrationTileMap[SoilHydration.Flooded]);
+				_tileLayer.SetCell(tileMapPosition, TileSetSourceId, _hydrationTileMap[SoilHydration.Flooded][randomSoilVariation.Next(8)]);
 				_logger.Debug($"Tile with hydration {hydration} is flooded");
 				break;
 		}
