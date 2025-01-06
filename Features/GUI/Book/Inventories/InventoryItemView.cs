d14443252fe2a9de0baeaa17f00aba41934deed1
kida31 <83455653+kida31@users.noreply.github.com>
2025-01-06 13:09:11 +0100

    Replace clickable property in item view.

--- Features/GUI/Book/Inventories/InventoryItemView.cs
+++ Features/GUI/Book/Inventories/InventoryItemView.cs
@@ -4,9 +4,9 @@ using untitledplantgame.Common;
 
 namespace untitledplantgame.Inventory.PlayerInventory.UI_InventoryItem;
 
-public partial class InventoryItemView : Control, ITooltipable
+public partial class InventoryItemView : Clickable, ITooltipable
 {
-	public Action Pressed;
+	// public Action Pressed;
 
 	[Export] private TextureRect _itemTextureRect;
 	[Export] private Label _displayItemName;
@@ -30,33 +30,8 @@ public partial class InventoryItemView : Control, ITooltipable
 		
 		FocusEntered += () =>
 		{
-			// EventBus.Instance.UiInventoryItemViewMoved(this);
 			_logger.Debug($"[{Name}] Entered");
 		};
-		// FocusExited += () =>
-		// {
-			// _logger.Debug($"[{Name}] Exited");
-		// };
-		
-		
-		// OLD
-		/*
-		
-		Connect(SignalName.MouseEntered, Callable.From(OnMouseEntered)); 
-		Connect(SignalName.MouseExited, Callable.From(OnMouseExited));
-		
-		FocusEntered += () =>
-		{
-			_itemTextureRect.Hide();
-			_logger.Debug($"[{Name}] Entered");
-		};
-
-		FocusExited += () =>
-		{
-			_itemTextureRect.Show();
-			_logger.Debug($"[{Name}] Exited");
-		};
-		*/
 	}
 
 	public void UpdateItemView(IItemStack itemStack)
@@ -76,71 +51,41 @@ public partial class InventoryItemView : Control, ITooltipable
 			_itemCurrentQuantity.Text = itemStack.Amount.ToString();
 			_itemTextureRect.Texture = itemStack.Icon;
 
-			TooltipText = itemStack.Name ?? "Placeholder_Tooltip";
+			// TooltipText = itemStack.Name ?? "Placeholder_Tooltip";
 
 			ItemStack = itemStack;
 		}
 	}
 
-	public override void _GuiInput(InputEvent @event)
-	{
-		if (@event is InputEventMouseButton button)
-		{
-			if (button.ButtonIndex == MouseButton.Left && button.Pressed)
-			{
-				Pressed?.Invoke();
-			}
-		}
-		
-		if (@event.IsActionPressed("ui_accept"))
-		{
-			Pressed?.Invoke();
-		}
-	}
+	// public override Variant _GetDragData(Vector2 atPosition)
+	// {
+	// 	// This is your custom method generating the preview of the drag data. Can be any Control node.
+	// 	var textureRect = new TextureRect();
+	// 	textureRect.Texture = _itemTextureRect.Texture;
+	// 	textureRect.PivotOffset = textureRect.Size / 2;
+	// 	SetDragPreview(textureRect);
+	// 	
+	// 	_itemTextureRect.Texture = null; // simulate drag; 
+	// 	return this;
+	// }
 
-	public void SetThisItemViewFocused()
-	{
-		GrabFocus();
-	}
+	// public override bool _CanDropData(Vector2 atPosition, Variant data)
+	// {
+	// 	return true;
+	// }
+	//
+	//
+	// public override void _DropData(Vector2 atPosition, Variant data)
 	
-	public override Variant _GetDragData(Vector2 atPosition)
-	{
-		// This is your custom method generating the preview of the drag data. Can be any Control node.
-		var textureRect = new TextureRect();
-		textureRect.Texture = _itemTextureRect.Texture;
-		textureRect.PivotOffset = textureRect.Size / 2;
-		SetDragPreview(textureRect);
-		
-		_itemTextureRect.Texture = null; // simulate drag; 
-		return this;
-	}
+	// {
+	// 	var previousSlot = (InventoryItemView) data;
+	// 	EventBus.Instance.InventoryItemMoved(previousSlot.ItemStack, EventBus.Instance.GetItemSlot()); // Remember, this has to be searched for!
+	// 	
+	// 	previousSlot.ClearItemViewData();
+	// }
 
-	public override bool _CanDropData(Vector2 atPosition, Variant data)
-	{
-		return true;
-	}
-	
-	
-	public override void _DropData(Vector2 atPosition, Variant data)
-	{
-		var previousSlot = (InventoryItemView) data;
-		EventBus.Instance.InventoryItemMoved(previousSlot.ItemStack, EventBus.Instance.GetItemSlot()); // Remember, this has to be searched for!
-		
-		previousSlot.ClearItemViewData();
-	}
-	
-	private void ClearItemViewData()
-	{
-		UpdateItemView(null);
-	}
-	
-	private void OnMouseEntered()
-	{
-		EventBus.Instance.SetItemSlot(this);
-	}
-
-	private void OnMouseExited()
-	{
-		EventBus.Instance.SetItemSlot(null);
-	}
+	// private void ClearItemViewData()
+	// {
+	// 	UpdateItemView(null);
+	// }
 }
