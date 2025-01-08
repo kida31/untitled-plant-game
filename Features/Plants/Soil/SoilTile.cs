7f31248f70e9e8a2319190c97eb7c1ff93f2d262
Elisa Z. <ez018@hdm-stuttgart.de>
2025-01-08 14:09:41 +0100

    Update SoilTile.cs

--- Features/Plants/Soil/SoilTile.cs
+++ Features/Plants/Soil/SoilTile.cs
@@ -75,7 +75,7 @@ public partial class SoilTile : Area2D, IWaterable
 	public void AddWater(float addedWater)
 	{
 		Hydration = Math.Min(Hydration + addedWater, MaxHydration);
-		HydrationChanged?.Invoke(addedWater, this);
+		HydrationChanged?.Invoke(Hydration, this);
 	}
 	
 	public void PlantSeed(Plant plant)
