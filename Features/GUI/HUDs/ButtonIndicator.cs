7b8d0c2c0da978962a711b3c605d1a5386c1d330
kida31 <83455653+kida31@users.noreply.github.com>
2025-01-15 16:15:59 +0100

    Add new button assets (partially)

--- Features/GUI/HUDs/ButtonIndicator.cs
+++ Features/GUI/HUDs/ButtonIndicator.cs
@@ -1,65 +1,228 @@
 using Godot;
-using System;
-using untitledplantgame.Common.Inputs;
+using untitledplantgame.Common;
 using untitledplantgame.Common.Inputs.GameActions;
 
+namespace untitledplantgame.GUI.HUDs;
+
+[Tool]
 public partial class ButtonIndicator : Control
 {
-	enum Button
+	private const string ButtonAssetsRoot = "res://Assets/UI/Buttons/";
+	private const string XboxButtonAssetsRoot = ButtonAssetsRoot + "XBOX/";
+	private const string PlaystationButtonAssetsRoot = ButtonAssetsRoot + "Playstation/";
+
+	private const GamepadType DefaultGamepad = GamepadType.Xbox;
+
+	enum GamepadType
 	{
-		
+		Xbox,
+		Playstation,
 	}
 	
 	private const string PressedAnimationName = "Pressed";
 	private const string ReleasedAnimationName = "Released";
 	
-	[Export] private Label _textLabel;
+	/// <summary>
+	/// Gamepad type for displayed button. This is a read only property
+	/// </summary>
+	[Export]
+	private GamepadType Gamepad
+	{
+		get => DefaultGamepad;
+		set
+		{
+			/* ignored */
+		}
+	}
+
+	[Export]
+	public JoyButton Button
+	{
+		get => _button;
+		set
+		{
+			_button = value;
+			OnButtonChanged();
+		}
+	}
+
+	[Export] private TextureButton _textureButton;
 	[Export] private AnimationPlayer _animationPlayer;
-	[Export] public string PrefabBoundAction = FreeRoam.TriggerLeft;
+
+	public bool IsPressed
+	{
+		get => _textureButton.IsPressed();
+		set => _textureButton.SetPressed(value);
+	}
+
+	private JoyButton _button;
+	private StringName _action;
+
+	private Logger _logger;
+	// private bool _isPressed = false;
 
 	public override void _Ready()
 	{
-		BindAction(PrefabBoundAction);
+		_logger = new Logger(this);
+		_action = ButtonAsAction(_button); // bind button
 	}
 
 	public override void _UnhandledInput(InputEvent @event)
 	{
-		if (@event.IsAction(_boundAction))
+		if (@event.IsAction(_action))
 		{
 			IsPressed = !@event.IsReleased();
 		}
 	}
 
-	public bool IsPressed
+	public override void _Input(InputEvent @event)
 	{
-		get => _isPressed;
-		set
+		if (@event is InputEventJoypadButton button)
 		{
-			if (_isPressed == value)
-			{
-				return;
-			}
-			
-			_isPressed = value;
-			_animationPlayer.Play(_isPressed ? PressedAnimationName : ReleasedAnimationName);
+			GD.Print(button.AsText());
 		}
 	}
-	
-	public void BindAction(string action)
-	{
-		_boundAction = action;
-		if (_boundAction != null)
-		{
 
+	private void OnButtonChanged()
+	{
+		_action = ButtonAsAction(_button);
+		AssignButtonImage(_button, Gamepad);
+	}
+
+	private StringName ButtonAsAction(JoyButton button)
+	{
+		switch (button)
+		{
+			case JoyButton.A:
+				return Base.South;
+			case JoyButton.B:
+				return Base.East;
+			case JoyButton.X:
+				return Base.West;
+			case JoyButton.Y:
+				return Base.North;
+
+			case JoyButton.Start:
+				return Base.Start;
+
+			case JoyButton.LeftShoulder:
+				return Base.BumperLeft;
+			case JoyButton.RightShoulder:
+				return Base.BumperRight;
+
+			case JoyButton.DpadUp:
+				return Base.Up;
+			case JoyButton.DpadDown:
+				return Base.Down;
+			case JoyButton.DpadLeft:
+				return Base.Left;
+			case JoyButton.DpadRight:
+				return Base.Right;
+			default:
+				_logger.Error("No action found for button: " + button);
+				return null;
 		}
 	}
-	
-	private string _boundAction;
-	private bool _isPressed = false;
-	
-	// Helper
-	private static Texture2D ButtonAsImage(JoyButton button)
+
+	private void AssignButtonImage(JoyButton button, GamepadType gamepadType)
 	{
-		return null;
+		switch (gamepadType)
+		{
+			case GamepadType.Xbox:
+				switch (button)
+				{
+					case JoyButton.A:
+						_textureButton.TextureNormal = TryLoadOrLogError(XboxButtonAssetsRoot + "A_default.png");
+						_textureButton.TexturePressed = TryLoadOrLogError(XboxButtonAssetsRoot + "A_pressed.png");
+						return;
+					case JoyButton.B:
+						_textureButton.TextureNormal = TryLoadOrLogError(XboxButtonAssetsRoot + "B_default.png");
+						_textureButton.TexturePressed = TryLoadOrLogError(XboxButtonAssetsRoot + "B_pressed.png");
+						return;
+					case JoyButton.X:
+						_textureButton.TextureNormal = TryLoadOrLogError(XboxButtonAssetsRoot + "X_default.png");
+						_textureButton.TexturePressed = TryLoadOrLogError(XboxButtonAssetsRoot + "X_pressed.png");
+						return;
+					case JoyButton.Y:
+						_textureButton.TextureNormal = TryLoadOrLogError(XboxButtonAssetsRoot + "Y_default.png");
+						_textureButton.TexturePressed = TryLoadOrLogError(XboxButtonAssetsRoot + "Y_pressed.png");
+						return;
+					case JoyButton.LeftShoulder:
+						_textureButton.TextureNormal = TryLoadOrLogError(XboxButtonAssetsRoot + "LB_default.png");
+						_textureButton.TexturePressed = TryLoadOrLogError(XboxButtonAssetsRoot + "LB_pressed.png");
+						return;
+					case JoyButton.RightShoulder:
+						_textureButton.TextureNormal = TryLoadOrLogError(XboxButtonAssetsRoot + "RB_default.png");
+						_textureButton.TexturePressed = TryLoadOrLogError(XboxButtonAssetsRoot + "RB_pressed.png");
+						return;
+					case JoyButton.Invalid:
+					case JoyButton.Back:
+					case JoyButton.Guide:
+					case JoyButton.Start:
+					case JoyButton.LeftStick:
+					case JoyButton.RightStick:
+					case JoyButton.DpadUp:
+					case JoyButton.DpadDown:
+					case JoyButton.DpadLeft:
+					case JoyButton.DpadRight:
+					case JoyButton.Misc1:
+					case JoyButton.Paddle1:
+					case JoyButton.Paddle2:
+					case JoyButton.Paddle3:
+					case JoyButton.Paddle4:
+					case JoyButton.Touchpad:
+					case JoyButton.SdlMax:
+					case JoyButton.Max:
+					default:
+						_logger.Error("Button not found: " + button);
+						return;
+				}
+			case GamepadType.Playstation:
+				switch (button)
+				{
+					case JoyButton.A:
+						return;
+					case JoyButton.Invalid:
+					case JoyButton.B:
+					case JoyButton.X:
+					case JoyButton.Y:
+					case JoyButton.Back:
+					case JoyButton.Guide:
+					case JoyButton.Start:
+					case JoyButton.LeftStick:
+					case JoyButton.RightStick:
+					case JoyButton.LeftShoulder:
+					case JoyButton.RightShoulder:
+					case JoyButton.DpadUp:
+					case JoyButton.DpadDown:
+					case JoyButton.DpadLeft:
+					case JoyButton.DpadRight:
+					case JoyButton.Misc1:
+					case JoyButton.Paddle1:
+					case JoyButton.Paddle2:
+					case JoyButton.Paddle3:
+					case JoyButton.Paddle4:
+					case JoyButton.Touchpad:
+					case JoyButton.SdlMax:
+					case JoyButton.Max:
+					default:
+						_logger.Error("Button not found: " + button);
+						return;
+				}
+			default:
+				_logger.Error($"Gamepad Button not found {gamepadType}::{button}");
+				return;
+		}
+	}
+
+	private Texture2D TryLoadOrLogError(string resourcePath)
+	{
+		var result = GD.Load<Texture2D>(resourcePath);
+		if (result == null)
+		{
+			_logger.Error("Failed to load resource: " + resourcePath);
+		}
+
+		return result;
 	}
 }
