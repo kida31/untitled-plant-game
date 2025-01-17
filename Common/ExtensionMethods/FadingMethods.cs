edff46105132aa9af90135a73023b469347179e3
kida31 <83455653+kida31@users.noreply.github.com>
2025-01-17 20:43:30 +0100

    add docs

--- Common/ExtensionMethods/FadingMethods.cs
+++ Common/ExtensionMethods/FadingMethods.cs
@@ -2,13 +2,32 @@
 
 namespace untitledplantgame.Common.ExtensionMethods;
 
+/// <summary>
+///     Methods to fade in and out a CanvasItem.
+/// </summary>
 public static class FadingMethods
 {
+	/// <summary>
+	///     Fades in the CanvasItem by tweening the alpha value of the modulate property.
+	///     The duration is the time it takes to fade in.
+	///     Returns the tween handler.
+	/// </summary>
+	/// <param name="self"></param>
+	/// <param name="duration"></param>
+	/// <returns></returns>
 	public static Tween FadeIn(this CanvasItem self, float duration)
 	{
 		return FadeAlpha(self, duration, 1.0f);
 	}
 
+	/// <summary>
+	///     Fades out the CanvasItem by tweening the alpha value of the modulate property.
+	///     The duration is the time it takes to fade out.
+	///     Returns the tween handler.
+	/// </summary>
+	/// <param name="self"></param>
+	/// <param name="duration"></param>
+	/// <returns></returns>
 	public static Tween FadeOut(this CanvasItem self, float duration)
 	{
 		return FadeAlpha(self, duration, 0.0f);
