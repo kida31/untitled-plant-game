e00e7f3cb7603d23d059c3e73064ebb9e30aa110
kida31 <83455653+kida31@users.noreply.github.com>
2025-01-20 16:43:40 +0100

    Catch NotConnected "Error" with comments

--- Features/GUI/Hooks/AutoExpandHook.cs
+++ Features/GUI/Hooks/AutoExpandHook.cs
@@ -1,11 +1,12 @@
 using Godot;
+using untitledplantgame.Common;
 
 namespace untitledplantgame.GUI.Hooks;
 
 /// <summary>
 ///		Add this node as a child of a Control node to make it expand in a way that keeps the aspect ratio
-///	 One dimension is chosen as primary dimension. The other dimension is then adjusted to keep the aspect ratio.
-/// Aspect ratio can be manually set initially or will be calculated from the size of the parent node in _Ready.
+///		One dimension is chosen as primary dimension. The other dimension is then adjusted to keep the aspect ratio.
+///		Aspect ratio can be manually set initially or will be calculated from the size of the parent node in _Ready.
 /// </summary>
 [Tool]
 public partial class AutoExpandHook : Node
@@ -27,10 +28,12 @@ public partial class AutoExpandHook : Node
 	[Export] private Mode _mode = Mode.HeightMatchWidth;
 
 	private Control _parent;
+	private Logger _logger;
 
-	public override void _Ready()
+	public override void _EnterTree()
 	{
 		_parent = GetParent<Control>();
+		_logger = new Logger(this);
 		if (_aspectRatio <= double.Epsilon)
 		{
 			var size = _parent.GetGlobalRect().Size;
@@ -43,6 +46,26 @@ public partial class AutoExpandHook : Node
 	public override void _ExitTree()
 	{
 		// Clean up
+
+		var callable = Callable.From(OnItemRectChanged);
+		var isConnected = _parent.IsConnected(CanvasItem.SignalName.ItemRectChanged, callable);
+		// Bad Practice Catch(anything) - This is a workaround for a (I assume) Godot bug
+		if (!isConnected)
+		{
+			// Why is it possible that the event is not added? It's in _Ready/_EnterTree
+			// [Tool] scripts are isolated from the rest of the scene tree.
+			// Funky stuff can happen when reloading scripts while not reloading scenes/editor
+
+			// C# sharp can unregister delegates without having connected earlier, no?
+			// This seems to be a Godot specific error.
+			// C# usually allows to remove a delegate from an event even if it is not added.
+			// Godot Signals do not allow that.
+
+			// This is probably editor-only and only during development (recompiling scripts)
+			_logger?.Error("OnItemRectChanged was not connected to ItemRectChanged signal. Disconnect suppressed.");
+			return;
+		}
+
 		_parent.ItemRectChanged -= OnItemRectChanged;
 	}
 
