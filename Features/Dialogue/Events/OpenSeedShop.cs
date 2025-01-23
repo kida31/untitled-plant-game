d596b636d202258518536c76465c44c00cf530a5
Zhang Elisa <ez018@hdm-stuttgart.de>
2025-01-23 20:13:08 +0100

    OpenseedShop and being confused

---
+++ Features/Dialogue/Events/OpenSeedShop.cs
@@ -0,0 +1,14 @@
+﻿using Godot;
+using untitledplantgame.Common;
+using untitledplantgame.Dialogue.Models;
+
+namespace untitledplantgame.Dialogue.Events;
+
+[GlobalClass]
+public partial class OpenSeedShop : DialogueEvent
+{
+	public override void Execute()
+	{
+		EventBus.Instance.SeedshopOpened();
+	}
+}
