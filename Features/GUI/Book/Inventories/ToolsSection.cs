145c5bcc14711d2d21a36c92a9ec86d28402a35b
kida31 <83455653+kida31@users.noreply.github.com>
2025-01-23 21:24:42 +0100

    Fix type in toolssection

--- Features/GUI/Book/Inventories/ToolsSection.cs
+++ Features/GUI/Book/Inventories/ToolsSection.cs
@@ -13,25 +13,27 @@ namespace untitledplantgame.GUI.Book.Inventories;
 /// </summary>
 public partial class ToolsSection : HBoxContainer
 {
-	[ExportGroup("Node Setup")]
-	[Export] private ToolBlobView _wateringCanBlob;
+	[ExportGroup("Node Setup")] [Export] private ToolCircle _wateringCanBlob;
 	[Export] private ToolCircle _shearsBlob;
 	[Export] private ToolCircle _shovelBlob;
 	[Export] private ToolCircle _fishingRod;
 	[Export] private ToolCircle _seedBagBlob;
-	
+
 	private Logger _logger;
 
 	public override void _Ready()
 	{
-		VisibilityChanged += OnVisibilityChanged;
 		_logger = new Logger(this);
+
+		if (_wateringCanBlob == null) _logger.Error("WateringCanBlob is null.");
+		if (_shearsBlob == null) _logger.Error("ShearsBlob is null.");
+		if (_shovelBlob == null) _logger.Error("ShovelBlob is null.");
+		if (_fishingRod == null) _logger.Error("FishingRod is null.");
+		if (_seedBagBlob == null) _logger.Error("SeedBagBlob is null.");
+
+		VisibilityChanged += OnVisibilityChanged;
+
 		_logger.Info("Ready");
-		if (_wateringCanBlob == null) _logger.Warn("WateringCanBlob is null.");
-		if (_shearsBlob == null) _logger.Warn("ShearsBlob is null.");
-		if (_shovelBlob == null) _logger.Warn("ShovelBlob is null.");
-		if (_fishingRod == null) _logger.Warn("FishingRod is null.");
-		if (_seedBagBlob == null) _logger.Warn("SeedBagBlob is null.");
 	}
 
 	private void OnVisibilityChanged()
@@ -39,16 +41,11 @@ public partial class ToolsSection : HBoxContainer
 		if (!IsVisibleInTree() || !IsNodeReady()) return;
 
 		var tools = Game.Player?.Toolbelt?.Tools;
-		if (_wateringCanBlob == null || _shearsBlob == null || _shovelBlob == null || _fishingRod == null || _seedBagBlob == null)
-		{
-			
-			GD.PushError("AWGAKLWGJLK");
-			_logger.Error("One or more ToolBlobViews are null.");
-		}
-		// _wateringCanBlob.Tool = tools?.FirstOrDefault(t => t is WateringCan);
-		_shearsBlob.Tool = tools?.FirstOrDefault(t => t is Shears);
-		_shovelBlob.Tool = tools?.FirstOrDefault(t => t is Shovel);
-		_fishingRod.Tool = tools?.FirstOrDefault(t => false /* is FishingRod */);
-		_seedBagBlob.Tool = tools?.FirstOrDefault(t => t is SeedBag);
+
+		if (_wateringCanBlob != null) _wateringCanBlob.Tool = tools?.FirstOrDefault(t => t is WateringCan);
+		if (_shearsBlob != null) _shearsBlob.Tool = tools?.FirstOrDefault(t => t is Shears);
+		if (_shovelBlob != null) _shovelBlob.Tool = tools?.FirstOrDefault(t => t is Shovel);
+		if (_fishingRod != null) _fishingRod.Tool = tools?.FirstOrDefault(t => false /* is FishingRod */);
+		if (_seedBagBlob != null) _seedBagBlob.Tool = tools?.FirstOrDefault(t => t is SeedBag);
 	}
 }
