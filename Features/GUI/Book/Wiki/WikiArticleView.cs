be6715dd6bc7f7011e53fc59fe47fd15c3fa5d01
kida31 <83455653+kida31@users.noreply.github.com>
2025-01-24 16:08:26 +0100

    revert changes

--- Features/GUI/Book/Wiki/WikiArticleView.cs
+++ Features/GUI/Book/Wiki/WikiArticleView.cs
@@ -4,25 +4,22 @@ using System.Linq;
 using Godot;
 using untitledplantgame.Database;
 using untitledplantgame.Inventory;
-using static untitledplantgame.Common.Inputs.GameActions.Book;
 
 namespace untitledplantgame.GUI.Book.Wiki;
 
 /// <summary>
 ///     This node is a control that displays a single article in the wiki.
 /// </summary>
-public partial class WikiArticleView : Control
+public partial class WikiArticleView : Node
 {
-	public Action ItemChanged;
-	
 	[Export] private TextureRect _iconTextureRect;
-	[Export] private ScrollTextBox _itemDescription;
+	[Export] private Label _itemDescription;
 	[Export] private Label _itemNameAndCategory;
 
 	// TODO show related items
 
 	private IItemStack _itemStack;
-	[Export] private Control _itemStats;
+	[Export] private Label _itemStats;
 	[Export] private WikiRelatedItemView[] _relatedItemViews = Array.Empty<WikiRelatedItemView>();
 	public event Action<IItemStack> RelatedItemClicked;
 
@@ -53,31 +50,6 @@ public partial class WikiArticleView : Control
 		}
 	}
 
-	public override void _UnhandledInput(InputEvent @event)
-	{
-		if (!IsVisibleInTree()) return;
-
-		if (!@event.IsAction(Common.Inputs.GameActions.Book.West))	return;
-
-		if (@event.IsReleased()) return;
-
-		SwitchPage();
-	}
-
-	private void SwitchPage()
-	{
-		if (_itemStats.Visible)
-		{
-			_itemStats.Hide();
-			_itemDescription.Show();
-		}
-		else
-		{
-			_itemStats.Show();
-			_itemDescription.Hide();
-		}
-	}
-
 	public void UpdateItemStack(IItemStack itemStack)
 	{
 		_itemStack = itemStack;
@@ -90,7 +62,7 @@ public partial class WikiArticleView : Control
 		_iconTextureRect.Texture = _itemStack?.Icon ?? null;
 		_itemNameAndCategory.Text = _itemStack != null ? $"{_itemStack.Name} - {_itemStack.Category.Name}" : "";
 		_itemDescription.Text = _itemStack?.WikiDescription ?? "";
-		// _itemStats.Text = "Stats!... \nStats!... \nStats!..."; // TODO:
+		_itemStats.Text = "Stats!... \nStats!... \nStats!..."; // TODO:
 
 		List<IItemStack> relatedItems = new();
 		if (_itemStack is ItemStack itemStack)
