0e68b0b6ae6126cbdb88733a768309dbd8c011ee
Jobonabas <jgehrung@web.de>
2025-01-28 14:01:33 +0100

    ADD: WalkingSFX Class, GetTyleType()

---
+++ Features/Audio/WalkingSFX.cs
@@ -0,0 +1,31 @@
+﻿using Godot;
+using untitledplantgame.Common;
+
+
+
+namespace untitledplantgame.Audio;
+public partial class WalkingSFX : Node
+{
+	public static WalkingSFX Instance { get; private set; }
+
+	private Node2D _playerPos;
+	private TileMapLayer _layer;
+	private string[,] sfx; 
+	
+	public override void _Ready()
+	{
+		
+	}
+
+	private void loadSFXPathsIntoArray() { 
+		
+	}
+
+	private int GetTileType() {
+		var playerPositionLocal = _playerPos.ToLocal(Game.Instance.GetPlayer().GlobalPosition);
+		var playerPositionOnMap = _layer.LocalToMap(playerPositionLocal);
+		var currentTile = _layer.GetCellTileData(playerPositionOnMap);
+		var tileType = currentTile.GetCustomData("Type").AsInt32();
+		return tileType;
+	}
+}
