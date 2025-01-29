93f6e54ae7a3d2c33cc2524b1af68f91c3e7a977
Zhang Elisa <ez018@hdm-stuttgart.de>
2025-01-29 17:22:20 +0100

    add fishing NPCs and Talkable fishing rod to start game

---
+++ Features/Dialogue/Events/OpenFishingGame.cs
@@ -0,0 +1,14 @@
+﻿using Godot;
+using untitledplantgame.Dialogue.Models;
+
+namespace untitledplantgame.Dialogue.Events;
+
+[GlobalClass]
+public partial class OpenFishingGame : DialogueEvent
+{
+	public override void Execute()
+	{
+		// Open the fishing game
+		// This is a placeholder for the actual implementation
+	}
+}
