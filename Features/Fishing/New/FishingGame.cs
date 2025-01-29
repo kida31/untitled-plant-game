f59dadd24e9041bdb85d72735ef7259916a57947
kida31 <83455653+kida31@users.noreply.github.com>
2025-01-29 01:43:16 +0100

    Move fishing pond logic to fishing game

--- Features/Fishing/New/FishingGame.cs
+++ Features/Fishing/New/FishingGame.cs
@@ -1,23 +1,43 @@
 using System;
 using Godot;
+using untitledplantgame.Common;
 
 namespace untitledplantgame.Fishing.New;
 
 public partial class FishingGame : Node
 {
+	public event Action GameWon;
+
 	[Export] private GameConfig _gameConfig;
-	[Export] private FishingPond _fishingPond;
+	[Export] private Area2D _fishingPond;
 	[Export] private ProgressBar _progressBar;
 	[Export] private FishingRod _fishingRod;
+	[Export] private PackedScene _fishPrefab;
+	[Export] private Marker2D _spawnPoint;
 
 	private float _progress;
 	private Fish _fish;
+	private Logger _logger;
 
 	public override void _Ready()
 	{
+		_logger = new(this);
 		_fishingRod.Initialize(_gameConfig.HookWidth, _gameConfig.HookSpeed, _gameConfig.HookSpeedAgainstFish);
+		_fishingPond.AreaExited += OnFishingPondExited;
 	}
 
+	public override void _Process(double delta)
+	{
+		if (_progress >= 100)
+		{
+			// Victory
+			_logger.Info("Victory!");
+			_fish.QueueFree();
+			_fish = null;
+			_progress = 0;
+			GameWon?.Invoke();
+		}
+	}
 	public override void _PhysicsProcess(double delta)
 	{
 		if (_fishingRod?.Fish != null && _fishingRod.ActiveDirection.X * _fish.ActiveDirection.X < 0)
@@ -28,9 +48,8 @@ public partial class FishingGame : Node
 		{
 			_progress -= (float)delta * _gameConfig.ProgressDecayPerSecond;
 		}
-	
-		_progress = Math.Clamp(_progress, 0, 100);
 
+		_progress = Math.Max(_progress, 0);
 
 		// Update UI
 		_progressBar.Value = _progress;
@@ -40,9 +59,32 @@ public partial class FishingGame : Node
 	{
 		if (@event is InputEventKey key && key.Pressed && key.Keycode == Key.F10)
 		{
-			var direction = Vector2.Zero;
-			direction.X = GD.Randi() - 0.5f;
-			_fish = _fishingPond.SpawnFish(_gameConfig.FishSpeed, _gameConfig.PullSpeed, direction);
+			_fish = SpawnFish();
 		}
+	}
+
+	public Fish SpawnFish()
+	{
+		var direction = Vector2.Zero;
+		direction.X = GD.Randf() - 0.5f;
+		direction = direction.Normalized();
+
+		_logger.Info($"Spawning a fish speed={_gameConfig.FishSpeed} pull={_gameConfig.PullSpeed} direction={direction}");
+
+		var fish = _fishPrefab.Instantiate<Fish>();
+		fish.Initialize(_gameConfig.FishSpeed, _gameConfig.PullSpeed, direction);
+
+		AddChild(fish);
+		fish.GlobalPosition = _spawnPoint.GlobalPosition;
+		return fish;
+	}
+
+	private void OnFishingPondExited(Area2D area)
+	{
+		if (area != _fish) return;
+
+		_fish.QueueFree();
+		_fish = null;
+		_logger.Info("Fish escaped!");
 	}
 }
--- Features/Fishing/New/FishingPond.cs
+++ Features/Fishing/New/FishingPond.cs
@@ -6,18 +6,5 @@ namespace untitledplantgame.Fishing.New;
 
 public partial class FishingPond: Area2D
 {
-    [Export] private PackedScene _fishPrefab;
-    [Export] private Marker2D _spawnPoint;
 
-    /// <summary>
-    ///     Spawns a fish in a random area
-    /// </summary>
-    public Fish SpawnFish(float speed, float speedOpposite, Vector2 direction) {
-        var fish = _fishPrefab.Instantiate<Fish>();
-        fish.Initialize(speed, speedOpposite, direction);
-
-        AddChild(fish);
-        fish.GlobalPosition = _spawnPoint.GlobalPosition;
-        return fish;     
-    }
 }
