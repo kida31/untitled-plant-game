9a27eb5abde72ef45432ab386421ec556860985d
sui-nyan <78491387+Sui-nyan@users.noreply.github.com>
2025-04-22 20:05:04 +0200

    cleaning up eventbus

--- Common/EventBus.cs
+++ Common/EventBus.cs
@@ -6,7 +6,6 @@ using untitledplantgame.Dialogue;
 using untitledplantgame.Dialogue.Models;
 using untitledplantgame.Inventory;
 using untitledplantgame.Plants;
-using untitledplantgame.Quests;
 using untitledplantgame.Shops;
 using untitledplantgame.Vending;
 
@@ -23,7 +22,7 @@ namespace untitledplantgame.Common;
 public partial class EventBus : Node
 {
 	public static EventBus Instance { get; private set; }
-	
+
 	private readonly Logger _logger = new("EventBus");
 
 	public override void _Ready()
@@ -39,19 +38,6 @@ public partial class EventBus : Node
 		}
 	}
 
-	//---------------------------------------------Legacy Signals---------------------------------------------
-	[Signal]
-	[Obsolete]
-	public delegate void NPCInteractedEventHandler(Node npc); //Replace with C# Action
-	
-	[Obsolete]
-	public void NotifyNPCInteracted(Node npc)
-	{
-		EmitSignal(nameof(NPCInteracted), npc);
-	}
-
-	//---------------------------------------------Legacy Signals---------------------------------------------
-	
 	public event Action OnSeedshopOpened;
 	public event Action OnInventoryOpen;
 
@@ -92,7 +78,7 @@ public partial class EventBus : Node
 	///     Emitted when a dialogue is started, passes the dialogue system
 	/// </summary>
 	public event Action<IDialogueSystem> InitialiseDialogue;
-	
+
 	public event Action<DialogueResourceObject> EndDialogue;
 
 	public void InvokeStartingDialogue(DialogueResourceObject obj)
@@ -104,7 +90,7 @@ public partial class EventBus : Node
 	{
 		InitialiseDialogue?.Invoke(obj);
 	}
-	
+
 	public void OnEndDialogue(DialogueResourceObject obj)
 	{
 		EndDialogue?.Invoke(obj);
@@ -114,7 +100,7 @@ public partial class EventBus : Node
 	public event Action<Plant> PlantHarvested;
 
 	public event Action<Plant> OnSeedPlanted;
-	
+
 	public void OnPlantHarvested(Plant obj)
 	{
 		PlantHarvested?.Invoke(obj);
@@ -126,24 +112,14 @@ public partial class EventBus : Node
 	}
 
 	//HUD
-	
+
 	public event Action<int, int> GoldChanged;
-	
-	// An event to change the portrait! Shouldn't be hard. But I don't know how to translate them into emotions
-	public event Action<AnimatedSprite2D, string> OnNpcStartDialogue;
-	
+
 	public void InvokeGoldChanged(int deltaGold, int newGold)
 	{
 		GoldChanged?.Invoke(deltaGold, newGold);
 	}
 
-
-	public void NpcDialogueWithPlayerStarted(AnimatedSprite2D portrait, string npcName)
-	{
-		OnNpcStartDialogue?.Invoke(portrait, npcName);
-	}
-	
-	
 	//Inventory
 
 	public event Action<int> OnFaithChange;
@@ -172,7 +148,7 @@ public partial class EventBus : Node
 
 	public event Action<Player.Player, IInventory> OnPlayerInventoryChanged;
 	public event Action<IItemStack> OnItemAddedToInventory;
-	
+
 	public void ItemAddedToInventory(IItemStack item)
 	{
 		OnItemAddedToInventory?.Invoke(item);
@@ -182,35 +158,23 @@ public partial class EventBus : Node
 	{
 		OnPlayerInventoryChanged?.Invoke(player, inventory);
 	}
-	
+
 	public event Action<ICraftingStation> BeforeCraftingStationUiOpened;
 
 	public void BeforeCraftingStationUiOpen(ICraftingStation craftingStation)
 	{
 		BeforeCraftingStationUiOpened?.Invoke(craftingStation);
 	}
-	
-	
-	
-	// Band-aid code for having actual things happening after selecting an answer.
-	
-	[Obsolete] // Now that's what I call a "WHAT DID I DO, WHERE DID MY STUFF GO?!?!?!?" panic moment
-	public event Action<string> OnResponseButtonPress;
-
-	[Obsolete] // Now that's what I call a "WHAT DID I DO, WHERE DID MY STUFF GO?!?!?!?" panic moment
-	public void ResponseButtonPressed(string message)
-	{
-		OnResponseButtonPress?.Invoke(message);
-	}
 
 	public event Action<IBgmArea> BgmAreaChanged;
+
 	public void InvokeBgmAreaChanged(IBgmArea area)
 	{
 		BgmAreaChanged?.Invoke(area);
 	}
-	
+
 	// Tool Events
-	
+
 	public event Action<SoilTile> WateredSoil;
 
 	public void OnWateredSoil(SoilTile obj)
