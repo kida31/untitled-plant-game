0bd2917bd98616e57f0de1bb0b613a13d4b970d1
Elisa <78491387+Sui-nyan@users.noreply.github.com>
2025-04-27 19:20:22 +0200

    357 improve tutorial (#361)
    Co-authored-by: lavenderaddict <177423441+lavenderaddict@users.noreply.github.com>

---
+++ Features/Dialogue/Events/StartQuestLine.cs
@@ -0,0 +1,15 @@
+﻿using Godot;
+using untitledplantgame.Dialogue.Models;
+using untitledplantgame.Quests;
+
+namespace untitledplantgame.Dialogue.Events;
+
+[GlobalClass]
+public partial class StartQuestLine : DialogueEvent
+{
+	[Export] private QuestLine QuestLine { get; set; }
+	public override void Execute()
+	{
+		QuestController.Instance.StartQuestLine(QuestLine);
+	}
+}
