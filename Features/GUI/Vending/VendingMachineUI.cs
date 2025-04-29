a9ebfd30e9f33fc6e8de0f85227a5f7216403850
kida31 <83455653+kida31@users.noreply.github.com>
2025-04-29 22:36:25 +0200

    Update VendingMachineUI.cs

--- Features/GUI/Vending/VendingMachineUI.cs
+++ Features/GUI/Vending/VendingMachineUI.cs
@@ -44,7 +44,7 @@ public partial class VendingMachineUI : Control
 		_emoteBubbleTimer.Timeout += OnEmoteBubbleTimeout;
 		AddChild(_emoteBubbleTimer);
 		_emoteBubble.FadeOut(0f);
-		_emoteBubble.Show();
+		
 
 		_itemSlots = _itemStackContainer.GetChildren().Cast<VendingItemView>().ToList();
 		_slider.ValueChanged += OnSliderValueChanged;
@@ -223,6 +223,7 @@ public partial class VendingMachineUI : Control
 		_vendingMachine.SetPriceSlider((float) value);
 
 		// NOTE: This does not correspond to the actual price or faith, but simply the slider position.
+		_emoteBubble.Show();
 		_emoteBubble.Value = (float) (_slider.Value / _slider.MaxValue);
 		_emoteBubbleTween?.Stop();
 		_emoteBubbleTween = _emoteBubble.FadeIn(_fadeInDuration);
