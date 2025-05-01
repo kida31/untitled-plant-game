0780f198171d71f72b5697552809665e4456ccab
sui-nyan <78491387+Sui-nyan@users.noreply.github.com>
2025-05-02 01:14:05 +0200

    Update MainMenu.cs

--- Features/GUI/HUDs/MainMenu/MainMenu.cs
+++ Features/GUI/HUDs/MainMenu/MainMenu.cs
@@ -5,16 +5,19 @@ using untitledplantgame.Common;
 public partial class MainMenu : Control
 {
 	private const string StartScene = "res://Scenes/OpeningScene.tscn";
+	private const string GameScene = "res://Main.tscn";
 	
 	[Export] private TextureButton _startButton;
 	[Export] private TextureButton _settingsButton;
 	[Export] private TextureButton _exitButton;
+	[Export] private bool _skipIntro;
 	
 	// Changed Aspect Ratio to: Keep
 	// The player shouldn't be able to change that (unless we want our game to look weird)
 	public override void _Ready()
 	{
-		_startButton.Pressed += () => GetTree().ChangeSceneToFile(StartScene);
+		
+		_startButton.Pressed += () => { GetTree().ChangeSceneToFile(_skipIntro ? StartScene : GameScene); };
 		_settingsButton.Pressed += OpenSettings;
 		_exitButton.Pressed += () => GetTree().Quit();
 		
