f2975d1d0be25cb1783d2e9551366c344cba9179
kida31 <83455653+kida31@users.noreply.github.com>
2025-05-03 00:13:51 +0200

    create remap button with logging window

---
+++ addons/upg_utils/Components/ProcessWindow.cs
@@ -0,0 +1,30 @@
+using Godot;
+
+public partial class ProcessWindow : AcceptDialog
+{
+    private string _log;
+    private TextEdit _textEdit;
+
+    public override void _EnterTree()
+    {
+        _textEdit = new TextEdit()
+        {
+            SizeFlagsHorizontal = Control.SizeFlags.ExpandFill,
+            SizeFlagsVertical = Control.SizeFlags.ExpandFill,
+            WrapMode = TextEdit.LineWrappingMode.Boundary,
+            AutowrapMode = TextServer.AutowrapMode.Arbitrary,
+            IndentWrappedLines = true,
+            ScrollSmooth = true,
+            Editable = false,
+            Text = _log,
+        };
+        AddChild(_textEdit);
+
+    }
+
+    public void PrintLine(string line)
+    {
+        _log += line + "\n";
+        if (_textEdit != null) _textEdit.Text = _log;
+    }
+}
